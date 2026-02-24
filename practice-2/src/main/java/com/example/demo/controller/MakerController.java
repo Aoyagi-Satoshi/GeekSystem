27ab4a39dc8895291dddd2c1b2c32e5ac6d45459
aoyag <aoyag@host.docker.internal>
2026-02-24 21:57:11 +0900

    メーカー一覧画面100％メーカー詳細画面80％残り削除機能

--- practice-2/src/main/java/com/example/demo/controller/MakerController.java
+++ practice-2/src/main/java/com/example/demo/controller/MakerController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.demo.entity.ItemEntity;
+import com.example.demo.entity.MakerEntity;
 import com.example.demo.service.MakerService;
 
 @Controller
@@ -14,9 +21,25 @@ public class MakerController {
 
 	@GetMapping("/makerList")
 	public String makerList(Model model) {
-	 
-	    model.addAttribute("makerlist", makerService.getAllMaker());
-	   
-	    return "MakerList";
+
+		model.addAttribute("makerlist", makerService.getAllMaker());
+
+		return "MakerList";
+	}
+
+	@GetMapping("/makers/{id}")
+	public String detailMaker(@PathVariable Long id, Model model) {
+		MakerEntity maker = makerService.getDetailMaker(id);
+		List<ItemEntity> items = makerService.getItemsByMakerId(id);
+		model.addAttribute("maker", maker);
+		model.addAttribute("items", items);
+		return "MakerDetail";
+	}
+
+	@GetMapping("/makers/{id}/delete")
+	@Transactional
+	public String delete(@PathVariable Long id) {
+		makerService.delete(id);
+		return "redirect:/adminList";
 	}
 }
